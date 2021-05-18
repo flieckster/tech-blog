0cea97ab9e7312e30827d704cf31d6925b4b1f13
Brian Flieck <brianflieck@gmail.com>
2021-05-18 15:30:14 -0400

    updated css and overall styling, adding in bootstrap elements

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,15 +1,11 @@
 async function deleteFormHandler(event) {
   event.preventDefault();
-
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
   const response = await fetch(`/api/posts/${id}`, {
-
-
     method: 'DELETE'
   });
-
   if (response.ok) {
     document.location.replace('/dashboard/');
   } else {
@@ -18,8 +14,4 @@ async function deleteFormHandler(event) {
   }
 }
 
-
-
-
-
-document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+document.querySelector('.btn-outline-danger').addEventListener('click', deleteFormHandler);
